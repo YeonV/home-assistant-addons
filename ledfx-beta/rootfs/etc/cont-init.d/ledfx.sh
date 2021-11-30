5799f69ea007b3d513cfc6b8f807bcc61c82b57a
YeonV <trusted.yeon@gmail.com>
2021-11-30 01:48:34 +0100

    ...

---
+++ ledfx-beta/rootfs/etc/cont-init.d/ledfx.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: ledfx
+# Configures ledfx
+# ==============================================================================
+
+if ! bashio::fs.directory_exists "/data/ledfx"; then
+    bashio::log "Creating config directory"
+    mkdir -p /data/ledfx
+fi
+
+mkdir -p /root/.ledfx
+
+#Generate config if not exist
+#if ! bashio::fs.file_exists "/data/ledfx/config.yaml"; then
+#    bashio::log "Creating default config"
+#    {
+#            echo "dev_mode: false"
+#            echo "devices: []"
+#            echo "host: 127.0.0.1"
+#            echo "max_workers: 10"
+#            echo "port: 8888"
+#            echo "presets: {}"
+#    } >> "/data/ledfx/config.yaml"
+#fi
