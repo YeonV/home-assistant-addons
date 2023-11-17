cec8a7e96e85ac6f233d052ca0c5edcb1bf2f8ad
spiro-c <spiroc@gmail.com>
2023-11-17 14:40:31 +1100

    Make dev addon for test

---
+++ ledfx-dev/rootfs/etc/cont-init.d/ledfx.sh
@@ -0,0 +1,12 @@
+#!/command/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: ledfx
+# Configures ledfx
+# ==============================================================================
+
+if ! bashio::fs.directory_exists "/data/ledfx"; then
+    bashio::log "Creating config directory"
+    mkdir -p /data/ledfx
+fi
+
+mkdir -p /root/.ledfx
