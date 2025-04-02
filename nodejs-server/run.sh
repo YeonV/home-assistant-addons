e62698e9f2d978c93481866bbf66368b2164f357
Yeon Vinzenz Varapragasam <YeonVinzenz.Varapragasam@trustedshops.de>
2025-04-02 18:54:50 +0200

    .

--- nodejs-server/run.sh
+++ nodejs-server/run.sh
@@ -1,31 +1,12 @@
-#!/usr/bin/with-contenv bashio
 
-# Exit on errors - keep this!
+#!/usr/bin/with-contenv bashio
+# ^ Mandatory shebang for S6 overlay + bashio helpers
+
+# Exit on errors
 set -e
 
-bashio::log.info "--- Simple run.sh: Starting up ---"
-echo "--- Simple run.sh: Logging to stdout ---" # Also try stdout
+bashio::log.info "Starting Node.js cam-reverse server..."
 
-# Keep the container alive for 5 minutes so we can see if it stays running
-# If the container stays up, the problem is likely with executing the node app.
-# If it still fails silently, the problem is more fundamental.
-sleep 300
-
-bashio::log.info "--- Simple run.sh: Finished sleeping (should only see this if testing sleep) ---"
-echo "--- Simple run.sh: Finished sleeping ---"
-
-# Comment out the real command for this test
-# exec node /usr/src/app/bin.cjs http_server
-
-
-# #!/usr/bin/with-contenv bashio
-# # ^ Mandatory shebang for S6 overlay + bashio helpers
-
-# # Exit on errors
-# set -e
-
-# bashio::log.info "Starting Node.js cam-reverse server..."
-
-# # Execute your Node.js application
-# # Use "exec" to replace the shell process with the Node process
-# exec node /usr/src/app/bin.cjs http_server
+# Execute your Node.js application
+# Use "exec" to replace the shell process with the Node process
+exec node /usr/src/app/bin.cjs http_server
